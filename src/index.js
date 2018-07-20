2d95fcb5a7e86dcbf128daca0cda43e1680f4a99
Cesar Martinez <cesar.martinez@globalgaming.com>
2018-07-20 13:33:48 +0200

    Implemented API fetch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import API_KEY from './config/keys';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    YTSearch({key: API_KEY, term: 'surfboard'}, (videos) =>
+      console.log(videos)
+    );
+  }
+
   render() {
     return (
-      <div className={'container'}>
+      <div className="container">
         <SearchBar/>
       </div>
     );
@@ -14,4 +23,3 @@ class App extends Component {
 }
 
 ReactDOM.render(<App/>, document.querySelector('#app'));
-
