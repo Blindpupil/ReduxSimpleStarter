cada2f137fa04fcdf50cc4150d9e010faa30c0d6
Cesar Martinez <cesar.martinez@globalgaming.com>
2018-07-20 17:40:00 +0200

    Video detail functionality and some styling

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect}) => {
   const videoList = videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video} />
+    return (
+      <VideoListItem onVideoSelect={onVideoSelect}
+        key={video.etag} video={video} />
+    );
   });
 
   return (
